603c0ab44eeb1b691417eb5192b827526664fb83
Bjarni <bjarnihaukur11@gmail.com>
2024-10-15 18:13:21 +0200

    train

--- train.py
+++ train.py
@@ -1,13 +1,11 @@
 import argparse
-from functools import partial
-from pathlib import Path
+from itertools import cycle
 
 import torch
 import torch.nn as nn
-from torch.utils.data import ConcatDataset, DataLoader, random_split
+from torch.utils.data import DataLoader
 
 import wandb
-import numpy as np
 from tqdm import tqdm
 
 torch.random.manual_seed(1337)
@@ -29,7 +27,13 @@ def main(args):
     train_dl = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True, prefetch_factor=args.prefetch_factor, num_workers=args.num_workers, persistent_workers=True)
     val_dl = DataLoader(val_ds, batch_size=args.batch_size, prefetch_factor=args.prefetch_factor, num_workers=args.num_workers, persistent_workers=True)
 
-
+    TOTAL_STEPS = len(train_dl) * args.epochs
+    VAL_INTERVAL = len(train_dl) // 10
+    VAL_STEPS = len(val_dl) // VAL_INTERVAL
+    
+    train_dl = cycle(train_dl)  # infinite iterator
+    val_dl = cycle(val_dl)
+    
     model = Song2Vec().to(DEVICE)
     optim = torch.optim.AdamW(model.parameters(), lr=args.lr)
     # scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=1, gamma=0.9)
@@ -48,63 +52,62 @@ def main(args):
             **vars(args)
         },
     )
-
         
     triplet_loss_fn = nn.TripletMarginLoss(margin=1.0, p=2)
 
     model = torch.compile(model, backend="aot_eager")
     model.train()
 
-    for epoch in range(args.epochs):
-        train_tqdm = tqdm(train_dl, desc=f"Epoch {epoch + 1}/{args.epochs} Training")
-
-        for i, (anchor, positive, negative) in enumerate(train_tqdm):
-            anchor, positive, negative = anchor.float(), positive.float(), negative.float() # shape (batch_size, 3, 1024, 2048)
+    train_loss, val_loss = float("inf"), float("inf")
+    step_tqdm = tqdm(range(TOTAL_STEPS), desc="Training...")
+    for step in step_tqdm:
+        step_tqdm.set_description(f"Training...")
+        anchor, positive, negative = next(train_dl)
+        anchor, positive, negative = anchor.float(), positive.float(), negative.float() # shape (batch_size, 3, 1024, 2048)
+        
+        with torch.autocast(device_type=DEVICE, dtype=DTYPE):
+            anchor_embed = model(anchor)
+            positive_embed = model(positive)
+            negative_embed = model(negative)
+        
+            # Compute Triplet Loss
+            loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
+        
+        # Backward pass
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        
+        train_loss = loss.item()
+        wandb.log({"train_loss": train_loss}, step=step)
+        step_tqdm.set_postfix(train_loss=train_loss, val_loss=val_loss)
+        
+        if step % VAL_STEPS == 0 and step != 0:
+            step_tqdm.set_description(f"Validating...")
+            model.eval()
+            total_val_loss = 0
             
-            with torch.autocast(device_type=DEVICE, dtype=DTYPE):
-                anchor_embed = model(anchor)
-                positive_embed = model(positive)
-                negative_embed = model(negative)
-            
-                # Compute Triplet Loss
-                loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
-            
-            # Backward pass
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-            
-            running_loss += loss.item()
-            
-            train_tqdm.set_postfix(train_loss=loss.item())
-            
-            if i % 10 == 9:  # Print every 10 mini-batches
-                wandb.log({"running_loss_train": running_loss / 10}, step=(epoch+1) * len(train_dl))
-                running_loss = 0.0
-
-        model.eval()
-        total_val_loss = 0
-        val_tqdm = tqdm(val_dl, desc=f"Epoch {epoch + 1}/{args.epochs} Validation")
-        for i, (anchor, positive, negative) in enumerate(val_tqdm):
-            anchor, positive, negative = anchor.float(), positive.float(), negative.float() # shape (batch_size, 3, 1024, 2048)
-            
-            with torch.autocast(device_type=DEVICE, dtype=DTYPE):
-                anchor_embed = model(anchor)
-                positive_embed = model(positive)
-                negative_embed = model(negative)
-            
-                # Compute Triplet Loss
-                loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
+            for _ in range(VAL_STEPS):
+                anchor, positive, negative = next(val_dl)
+                anchor, positive, negative = anchor.float(), positive.float(), negative.float() # shape (batch_size, 3, 1024, 2048)
                 
-            total_val_loss += loss.item()
-            
-            val_tqdm.set_postfix(val_loss=loss.item())
+                with torch.autocast(device_type=DEVICE, dtype=DTYPE):
+                    anchor_embed = model(anchor)
+                    positive_embed = model(positive)
+                    negative_embed = model(negative)
                 
-        wandb.log({"avg_val_loss": total_val_loss / len(val_dl)}, step=(epoch+1) * len(train_dl)) # to get it on the same axis
+                    # Compute Triplet Loss
+                    loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
+                
+                val_loss = loss.item()
+                step_tqdm.set_postfix(train_loss=train_loss, val_loss=val_loss)
+                total_val_loss += val_loss
+            
+            wandb.log({"val_loss": total_val_loss / VAL_STEPS}, step=step)
 
-        torch.save({
-            'model_state_dict': model.state_dict()
-        }, f"epoch_{epoch + 1}.pt")
+    torch.save({
+        'model_state_dict': model.state_dict()
+    }, f"epoch_{epoch + 1}.pt")
 
 
 if __name__ == "__main__":
