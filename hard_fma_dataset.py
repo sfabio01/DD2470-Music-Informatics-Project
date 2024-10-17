234c11223edb2a1f8697d6fb31746f5f2c113e55
Fabio Sabbion <>
2024-10-17 17:38:49 +0200

    aaa

--- hard_fma_dataset.py
+++ hard_fma_dataset.py
@@ -1,13 +1,13 @@
+import json
+from os.path import join as pjoin
+from typing import Optional, Tuple, Dict
+
 import torch
 from torch.utils.data import Dataset
-import pandas as pd
-import os
-import utils
-import json
 import numpy as np
-from os.path import join as pjoin
-from typing import Optional, Tuple, Dict, List
-from functools import lru_cache
+import pandas as pd
+
+import utils
 
 # Define constants at module level
 METADATA_INDEX = ['genre', 'interest', 'year_created']
@@ -16,13 +16,21 @@ INTEREST_BINS_LABELS = ['0-2000', '2000-5000', '5000-10000', '10000+']
 YEAR_BINS = [2007, 2013, 2018]
 YEAR_BINS_LABELS = ['2008-2012', '2013-2017']
 
+
 class FmaDataset(Dataset):
-    def __init__(self, metadata_folder: str, root_dir: str, transform: Optional[callable] = None):
+    def __init__(self, metadata_folder: str, root_dir: str, split: str, transform: Optional[callable] = None, skip_sanity_check: bool = False):
+        assert split in ['train', 'val'], "Split must be one of 'train' or 'val'"
+
+        self.split = split
+
         # Load data only once during initialization
         self.tracks = utils.load(pjoin(metadata_folder, 'tracks.csv'))
         
         # Load metadata dictionaries using a helper method
-        self.metadata_dicts = self._load_metadata_dicts(metadata_folder)
+        self.metadata_dicts = self._load_metadata_dicts(pjoin(metadata_folder, split))
+        self.train_val_splits = self._load_train_val_splits(metadata_folder)
+
+        if not skip_sanity_check: assert self._sanity_check()
         
         # Preprocess tracks once during initialization
         self.small = self._preprocess_tracks()
@@ -34,6 +42,14 @@ class FmaDataset(Dataset):
         # Pre-calculate valid indices for each category
         self._initialize_category_indices()
 
+    def _sanity_check(self) -> bool:
+        """Check if all metadata dictionaries contains the same number of tracks."""
+        len_genre = sum(len(tracks) for tracks in self.metadata_dicts['genre'].values())
+        len_interest = sum(len(tracks) for tracks in self.metadata_dicts['interest'].values())
+        len_year_created = sum(len(tracks) for tracks in self.metadata_dicts['year_created'].values())
+
+        return len_genre == len_interest == len_year_created
+
     def _load_metadata_dicts(self, metadata_folder: str) -> Dict:
         """Load all metadata dictionaries at once."""
         return {
@@ -42,12 +58,17 @@ class FmaDataset(Dataset):
             'year_created': json.load(open(pjoin(metadata_folder, 'year_created_bin_dict.json')))
         }
 
+    def _load_train_val_splits(self, metadata_folder: str) -> Dict:
+        """Load the train-val splits."""
+        return json.load(open(pjoin(metadata_folder, 'train_val_ids.json')))
+
     def _preprocess_tracks(self) -> pd.DataFrame:
         """Preprocess the tracks DataFrame."""
         small = self.tracks[self.tracks['set', 'subset'] <= 'small'].copy()
-        small = small['track']
+        small = small['track'].reset_index(drop=False)
         small['year_created'] = small['date_created'].dt.year
         small = small.rename(columns={'genre_top': 'genre'})
+        small = small[~small['track_id'].isin([133297, 99134, 108925])] # remove corrupted tracks
         return small.reset_index(drop=False)
 
     def _initialize_category_indices(self):
@@ -59,16 +80,16 @@ class FmaDataset(Dataset):
         }
 
     def __len__(self) -> int:
-        return len(self.small)
+        return len(self.train_val_splits[self.split])
 
     def _load_track(self, track_id: str) -> np.ndarray:
-        """Load and cache track data."""
+        """Load track data from npy file."""
         return np.load(pjoin(self.root_dir, f'{track_id.zfill(6)}.npy'))
     
     def _load_tracks(self, track_ids: str) -> np.ndarray:
         """Load multiple tracks and concatenate them."""
         return np.concatenate([self._load_track(track_id) for track_id in track_ids])
-
+        
     def _get_bin_for_value(self, value: float, category: str) -> str:
         """Get the appropriate bin for a value in a category."""
         if category == 'interest':
@@ -109,9 +130,9 @@ class FmaDataset(Dataset):
         
         # Load and transform samples
         samples = [
-            self._load_track(track_id),
-            self._load_tracks(positive_ids),
-            self._load_tracks(negative_ids)
+            torch.tensor(self._load_track(track_id)),
+            torch.tensor(self._load_tracks(positive_ids)),
+            torch.tensor(self._load_tracks(negative_ids))
         ]
         
         if self.transform:
@@ -119,6 +140,5 @@ class FmaDataset(Dataset):
             for i in range(len(samples[1])):
                 samples[1][i] = self.transform(samples[1][i])
                 samples[2][i] = self.transform(samples[2][i])
-    
-            
+
         return tuple(samples)
