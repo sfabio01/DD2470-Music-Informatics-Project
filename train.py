d9c379defa218ebb90f3468e60255520eedc71fa
Bjarni Haukur <bjarnihaukur11@gmail.com>
2024-10-17 22:06:58 +0200

    bugs

--- train.py
+++ train.py
@@ -72,13 +72,13 @@ def main(args):
         anchor, positive, negative = anchor.to(DEVICE), positive.to(DEVICE), negative.to(DEVICE)
         
         with torch.autocast(device_type=DEVICE, dtype=DTYPE, enabled=DEVICE=="cuda"):
-            anchor, anchor_embed = model(anchor)
-            positive, positive_embed = model(positive)
-            negative, negative_embed = model(negative)
+            anchor_out, anchor_embed = model(anchor)
+            _, positive_embed = model.encode(positive)  # no need to decode positive / negative
+            _, negative_embed = model.encode(negative)
         
             # Compute Triplet Loss
             triplet_loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
-            reconstruction_loss = reconstruction_loss_fn(anchor, anchor_embed)
+            reconstruction_loss = reconstruction_loss_fn(anchor_out, anchor)
             loss = triplet_loss + reconstruction_loss
         
         # Backward pass
@@ -106,9 +106,9 @@ def main(args):
                 anchor, positive, negative = anchor.to(DEVICE), positive.to(DEVICE), negative.to(DEVICE)
                 
                 with torch.autocast(device_type=DEVICE, dtype=DTYPE, enabled=DEVICE=="cuda"):
-                    anchor, anchor_embed = model(anchor)
-                    positive, positive_embed = model(positive)
-                    negative, negative_embed = model(negative)
+                    anchor_out, anchor_embed = model(anchor)
+                    _, positive_embed = model.encode(positive)
+                    _, negative_embed = model.encode(negative)
                 
                     # Save anchor embeddings
                     # embeddings.extend(anchor_embed.detach().cpu().tolist())
@@ -117,7 +117,7 @@ def main(args):
                 
                     # Compute Triplet Loss
                     triplet_loss = triplet_loss_fn(anchor_embed, positive_embed, negative_embed)
-                    reconstruction_loss = reconstruction_loss_fn(anchor, anchor_embed)
+                    reconstruction_loss = reconstruction_loss_fn(anchor_out, anchor)
                     loss = triplet_loss + reconstruction_loss
                     
                     positive_cosine_distance = F.cosine_similarity(anchor_embed, positive_embed)
