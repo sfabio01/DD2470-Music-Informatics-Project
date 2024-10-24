9d6cf8ccd1db254ff8301354669e567baadea83e
Bjarni <bjarnihaukur11@gmail.com>
2024-10-24 18:40:12 +0200

    simplified and downsample

--- baseline_model.py
+++ baseline_model.py
@@ -63,23 +63,16 @@ class Song2Vec(nn.Module):
             bidirectional=False,
             dropout=0.3  # Dropout between GRU layers
         )
-
-        self.hid_mapping = nn.Sequential(
-            nn.Linear(512, 256 * 2),
-            nn.Dropout(p=0.5)
-        )
+        
+        self.enc_mapping = nn.Linear(512, 256)
 
         self.gru_decoder = nn.GRU(
             input_size=1,
-            hidden_size=256,
+            hidden_size=512,
             num_layers=2,
             batch_first=True,
             bidirectional=False,
             dropout=0.3  # Dropout between GRU layers
-        )
-        self.fc_dec = nn.Sequential(
-            nn.Linear(in_features=256, out_features=256),
-            nn.Dropout(p=0.5)
         )
         self.decoder = CNNDecoder()
         
@@ -90,28 +83,23 @@ class Song2Vec(nn.Module):
         B, H_enc, W_enc = x.shape
         x = x.permute(0, 2, 1).contiguous()  # Shape: (B, W_enc, H_enc)
         
-        out, h_n = self.gru_encoder(x)  # h_n: (num_layers, B, hidden_size)
-        z = h_n[-1]  # Only use last layer's hidden state
-        return z, W_enc  # Return z and sequence length for decoder
+        _, h_n = self.gru_encoder(x)  # h_n: (num_layers, B, hidden_size)
+
+        z = self.enc_mapping(h_n[-1])
+        return z, h_n, W_enc  # Return z and sequence length for decoder
     
-    def decode(self, z, seq_len):
-        # Map encoder hidden state z to decoder initial hidden state h_0
-        h_0 = self.hid_mapping(z)  # Shape: (B, num_layers * decoder_hidden_size)
-        h_0 = h_0.view(z.shape[0], self.gru_decoder.num_layers, self.gru_decoder.hidden_size)
-        h_0 = h_0.permute(1, 0, 2).contiguous()  # Shape: (num_layers, B, decoder_hidden_size)
-        
+    def decode(self, h_n, seq_len):        
         # Prepare decoder inputs (zeros)
-        decoder_inputs = torch.zeros(z.shape[0], seq_len, 1).to(z.device)  # Input size is 1
+        decoder_inputs = torch.zeros(h_n.shape[1], seq_len, 1).to(h_n.device)  # Input size is 1
         
         # Run decoder GRU
-        out, _ = self.gru_decoder(decoder_inputs, h_0)  # out: (B, seq_len, hidden_size)
-        out = self.fc_dec(out)  # Shape: (B, seq_len, 256)
+        out, _ = self.gru_decoder(decoder_inputs, h_n)  # out: (B, seq_len, hidden_size)
         x_reconstructed = self.decoder(out.permute(0, 2, 1))  # Adjust dimensions for CNNDecoder
         return x_reconstructed
     
     def forward(self, x):
-        z, seq_len = self.encode(x)
-        x_reconstructed = self.decode(z, seq_len)
+        z, h_n, seq_len = self.encode(x)
+        x_reconstructed = self.decode(h_n, seq_len)
         x_reconstructed = x_reconstructed.permute(0, 2, 3, 1)  # Shape: (B, H, W, C)
         return x_reconstructed, z
 
